7ec81ccc66b80e8514b70583a7d5521e212fa275
Pradeep92hv <hvpradeep92@gmail.com>
2024-06-05 17:42:48 +0530

    step 58: Connecting angular Frontend With Todo List Restfull service

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoDataService } from '../service/data/todo-data.service';
 
 export class Todo{
   constructor(
@@ -20,16 +21,28 @@ export class Todo{
 
 
 export class ListTodosComponent implements OnInit {
-  todos=[
-    new Todo(1,"Learn to dance",false,new Date()),
-    new Todo(2,"Learn to song",false,new Date()),
-    new Todo(2,"Learn to eat",false,new Date())
+  // todos=[
+  //   new Todo(1,"Learn to dance",false,new Date()),
+  //   new Todo(2,"Learn to song",false,new Date()),
+  //   new Todo(2,"Learn to eat",false,new Date())
 
-   ]
+  //  ]
 
-  constructor() { }
+  todos:Todo[]
+  constructor(
+    private todosService: TodoDataService
+  ) { }
 
   ngOnInit() {
+    this.todosService.retrieveAllTodos("pradeephv").subscribe(
+      response => {
+        console.log(response)
+        this.todos=response
+      },
+      error => {
+        console.error('Error fetching todos', error);
+      }
+    )
   }
 
 }
